bbc753b7eb0f6f85488777acf604e7e731030d80
Grigory <rogerg@bk.ru>
2017-12-01 20:48:38 -0800

    gunicorn

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def app(environ, start_response):
 	start_response("200 OK", [
 		("Content-Type", "text/plain")
-		]
-	resp = environ['QUERY_STRING'].split('&")
+		])
+	resp = environ['QUERY_STRING'].split("&")
 	resp = [item + '\r\n' for item in resp]
 	return resp
