8425bcd3e2596e8e4a3614801b0e3a778bc571a1
Grigory <rogerg@bk.ru>
2017-12-02 19:02:23 -0800

    django

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
+# def app(environ, start_response):
+#     start_response("200 OK", [
+#         ("Content-Type", "text/plain")
+#     ])
+#     resp = environ['QUERY_STRING'].split("&")
+#     resp = [item + '\r\n' for item in resp]
+#     return resp
+
+
 def app(environ, start_response):
-	start_response("200 OK", [
-		("Content-Type", "text/plain")
-		])
-	resp = environ['QUERY_STRING'].split("&")
-	resp = [item + '\r\n' for item in resp]
-	return resp
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
