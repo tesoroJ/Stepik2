6dce0b554032189d966947578a39d9307dffed9b
Grigory <rogerg@bk.ru>
2017-12-02 20:06:53 -0800

    sfsdf

--- hello.py
+++ hello.py
@@ -1,13 +1,13 @@
-# def app(environ, start_response):
-#     start_response("200 OK", [
-#         ("Content-Type", "text/plain")
-#     ])
-#     resp = environ['QUERY_STRING'].split("&")
-#     resp = [item + '\r\n' for item in resp]
-#     return resp
-
-
 def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
-                  encoding="utf8")]
+    start_response("200 OK", [
+        ("Content-Type", "text/plain")
+    ])
+    resp = environ['QUERY_STRING'].split("&")
+    resp = [item + '\r\n' for item in resp]
+    return resp
+
+
+# def app(environ, start_response):
+#     start_response('200 OK', [('Content-Type', 'text/plain')])
+#     return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+#                   encoding="utf8")]
