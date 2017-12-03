dd059684521514a439645ab5cb797ba8865e1b6d
Grigory <rogerg@bk.ru>
2017-12-02 17:24:50 -0800

    django

---
+++ ask/qa/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url,include
+from django.contrib import admin
+from . import views
+
+
+urlpatterns = [
+    url(r'^$', views.test),
+    url(r'login/', views.test),
+    url(r'signup', views.test),
+    url(r'question/(?P<pk>)/', views.test),
+    url(r'ask/', views.test),
+    url(r'popular/', views.test),
+    url(r'new/', views.test),
+]
