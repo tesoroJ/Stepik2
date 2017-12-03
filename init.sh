a8df3261143d1d599a433acfbde0a06a9e7c3adc
Grigory <rogerg@bk.ru>
2017-12-02 18:40:25 -0800

    django

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/testdj
 sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
