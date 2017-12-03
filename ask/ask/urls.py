92b786e50c42e8bece6b20102afc8be5989054a4
Grigory <rogerg@bk.ru>
2017-12-02 20:33:02 -0800

    dfdsf

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from qa import views
 
 
 urlpatterns = [
-    url(r'^$', include('qa.urls')),
+    url(r'^$', views.test),
+    url(r'^login/', views.test),
+    url(r'^signup', views.test),
+    url(r'^question/(?P<id>[0-9]+)/', views.test),
+    url(r'^ask/', views.test),
+    url(r'^popular/', views.test),
+    url(r'^new/', views.test),
     url(r'^admin/', admin.site.urls),
 ]
+
+
