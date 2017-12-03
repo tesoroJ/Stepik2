59f2f7fa379b1f7e61cdfc1286c65f8f4f2659f8
Grigory <rogerg@bk.ru>
2017-12-02 20:02:51 -0800

    sfsdf

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.test),
-    url(r'login/', views.test),
-    url(r'signup', views.test),
-    url(r'question/(?P<id>[0-9]+)/', views.test),
-    url(r'ask/', views.test),
-    url(r'popular/', views.test),
-    url(r'new/', views.test),
+    url(r'^login/', views.test),
+    url(r'^signup', views.test),
+    url(r'^question/(?P<id>[0-9]+)/', views.test),
+    url(r'^ask/', views.test),
+    url(r'^popular/', views.test),
+    url(r'^new/', views.test),
 ]
