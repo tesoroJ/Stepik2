ff8ac55afe7ef87e92fad09438ed48f408121751
Grigory <rogerg@bk.ru>
2017-12-06 13:27:49 -0800

    hello

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,11 +1,60 @@
+# # -*- coding: utf-8 -*-
+# #from __future__ import unicode_literals
+# from django.db import models
+# from django.contrib.auth.models import User
+# # Create your models here.
+#
+#
+# class QuestionManager(models.Model):
+#     def new(self):
+#         return self.order_by('-added_at')
+#
+#     def popular(self):
+#         return self.order_by('-rating')
+#
+#
+# class Question(models.Model):
+#     objects = QuestionManager()
+#     title = models.CharField(max_length=255)
+#     text = models.TextField()
+#     added_at = models.DateTimeField(blank=True, auto_now=True)
+#     rating = models.IntegerField(default=0)
+#     author = models.ForeignKey(User)
+#     likes = models.ManyToManyField(User, related_name='question_like_user')
+#
+#     # def __unicode__(self):
+#     #     return self.title
+#
+#     def __str__(self):
+#         return self.title
+#
+#     def get_absolute_url(self):
+#         return '/question/%d/' % self.pk
+#
+#     # objects = QuestionManager()
+#
+#     class Meta:
+#         ordering = ['-id']
+#
+#
+# class Answer(models.Model):
+#     text = models.TextField()
+#     added_at = models.DateTimeField(blank=True, auto_now=True)
+#     question = models.ForeignKey(Question)
+#     author = models.ForeignKey(User)
+
 # -*- coding: utf-8 -*-
-#from __future__ import unicode_literals
+from __future__ import unicode_literals
+
 from django.db import models
+
 from django.contrib.auth.models import User
+
+
 # Create your models here.
 
 
-class QuestionManager(models.Model):
+class QuestionManager(models.Manager):
     def new(self):
         return self.order_by('-added_at')
 
@@ -14,7 +63,7 @@ class QuestionManager(models.Model):
 
 
 class Question(models.Model):
-    objects = QuestionManager()
+    # objects = QuestionManager()
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(blank=True, auto_now=True)
@@ -22,16 +71,13 @@ class Question(models.Model):
     author = models.ForeignKey(User)
     likes = models.ManyToManyField(User, related_name='question_like_user')
 
-    # def __unicode__(self):
-    #     return self.title
-
-    def __str__(self):
+    def __unicode__(self):
         return self.title
 
     def get_absolute_url(self):
         return '/question/%d/' % self.pk
 
-    # objects = QuestionManager()
+    objects = QuestionManager()
 
     class Meta:
         ordering = ['-id']
